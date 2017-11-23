92e98cc2c898cd27d86aea716bd6988a1b7e1b68
hd <kimi2009@vip.qq.com>
2017-11-23 09:31:40 +0800

    系统评分数据1

--- app/src/main/java/itor/topnetwork/com/dxditor/test/studyKotlin.kt
+++ app/src/main/java/itor/topnetwork/com/dxditor/test/studyKotlin.kt
@@ -1,9 +1,17 @@
 package itor.topnetwork.com.dxditor.test
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 /**
  * Created by D.Han on 2017/11/15.
  */
 fun main(args:Array<String>){
     println("hello world")
+    println(SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()))
+    val a=29
+    print(a::class)
+
+
 }
